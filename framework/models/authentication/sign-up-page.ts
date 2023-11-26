4b227e53c29c16c8e6d527b119b1da483f76ddf7
Vladyslav Shcherbyna <vladislav.scherbina2017@gmail.com>
2023-11-26 17:47:08 +0200

    Create POM for Sign Up and create some basic Sign Up page

---
+++ framework/models/authentication/sign-up-page.ts
@@ -0,0 +1,66 @@
+import { Locator, Page, expect } from '@playwright/test'
+import routes from '@test-data/routes.json'
+import text from '@test-data/text.json'
+export default class SignUpPage {
+    readonly page: Page
+
+
+    readonly firstNameFld: Locator
+    readonly lastNameFld: Locator
+    readonly emailFld: Locator
+    readonly telephoneFld: Locator
+    readonly passwordFld: Locator
+    readonly confirmPassowrdFld: Locator
+    readonly yesSubscribeRadioBtn: Locator
+    readonly privacyPolicyCheckbox: Locator
+    readonly continueBtn: Locator
+
+    readonly firstNameFldError: Locator
+    readonly lastNameFldError: Locator
+    readonly emailFldError: Locator
+    readonly telephoneFldError: Locator
+    readonly passwordFldError: Locator
+    readonly warningError: Locator
+    readonly createdAccounLbl: Locator
+
+
+    constructor(page: Page) {
+        this.page = page
+        this.firstNameFld = page.locator('#input-firstname')
+        this.lastNameFld = page.locator('#input-lastname') 
+        this.emailFld = page.locator('#input-email')
+        this.telephoneFld = page.locator('#input-telephone')
+        this.passwordFld = page.locator('#input-password')
+        this.confirmPassowrdFld = page.locator('#input-confirm')
+        this.yesSubscribeRadioBtn = page.locator('(//input[@name="newsletter"])[1]')
+        this.privacyPolicyCheckbox = page.locator('input[name="agree"]')
+        this.continueBtn = page.getByRole('button', { name: 'Continue' })
+
+        this.firstNameFldError = page.getByText('First Name must be between 1 and 32 characters!')
+        this.lastNameFldError = page.getByText('Last Name must be between 1 and 32 characters!')
+        this.emailFldError = page.getByText('E-Mail Address does not appear to be valid!')
+        this.telephoneFldError = page.getByText('Telephone must be between 3 and 32 characters!')
+        this.passwordFldError = page.getByText('Password must be between 4 and 20 characters!')
+        this.warningError = page.getByText(' Warning: You must agree to the Privacy Policy!')
+        this.createdAccounLbl = page.getByText('Your Account Has Been Created!')
+    }
+
+
+    async successSignUp(firstName: string, lastName: string,
+        email: string, telephone: string,
+        password: string) {
+
+        await this.firstNameFld.fill(firstName)
+        await this.lastNameFld.fill(lastName)
+        await this.emailFld.fill(email)
+        await this.telephoneFld.fill(telephone)
+        await this.passwordFld.fill(password)
+        await this.confirmPassowrdFld.fill(password)
+        await this.yesSubscribeRadioBtn.click()
+        await expect(this.yesSubscribeRadioBtn).toBeChecked()
+        await this.privacyPolicyCheckbox.check()
+        await expect(this.privacyPolicyCheckbox).toBeChecked()
+        await this.continueBtn.click()
+        await expect(this.page).toHaveURL(/.*success/);
+    }
+}
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,114 +1,92 @@
-import type { PlaywrightTestConfig } from '@playwright/test'
-import { devices } from '@playwright/test'
-import path from 'path'
-import credsFile from '@test-data/creds.json'
+import { defineConfig, devices } from '@playwright/test';
 
-export const STORAGE_STATE = path.join(__dirname, 'test-data/storage-state.json');
-const creds = process.env.ENV === 'staging' ? credsFile.staging : credsFile.prod
+export default defineConfig({
+  testDir: 'tests/e2e',
 
-const config: PlaywrightTestConfig = {
-  workers: 1,
-  retries: 2,
-  timeout: 60 * 1000,
+  fullyParallel: true,
   forbidOnly: true,
-  outputDir: 'test-results/',
-  reporter: [['html', { open: 'always', outputFolder: './test-report' }]],
-
-  // Ignores serial notation; consider disabling this in case of flakiness
-  fullyParallel: false,
-
-  expect: {
-    timeout: 15 * 1000,
-
-    // Allowed screenshot difference; should be supervised to prevent false positives
-    toHaveScreenshot: { maxDiffPixelRatio: 0.1 }
-  },
+  retries: 0,
+  workers: 1,
+  reporter: 'html',
 
   use: {
-    actionTimeout: 15 * 1000,
-    baseURL:
-      process.env.ENV === 'staging'
-        ? 'https://playwright.dev/docs/intro'
-        : 'https://playwright.dev/docs/intro',
-    screenshot: 'only-on-failure',
+    baseURL: 'https://demo-opencart.com/index.php?',
     trace: 'retain-on-failure',
-    video: 'retain-on-failure',
-
-    viewport: { height: 1600, width: 900 },
-
-    httpCredentials: {
-      username: creds.http_username,
-      password: creds.http_password
-    },
+    headless: false
   },
-
   projects: [
     {
-      name: 'setup',
-      testMatch: /global-setup\.ts/,
-      teardown: 'teardown',
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'teardown',
-      testMatch: /global-teardown\.ts/,
-      use: {
-        storageState: STORAGE_STATE
-      }
-    },
-    {
-      name: 'chrome',
-      use: {
-        ...devices['Desktop Chrome'],
-        storageState: STORAGE_STATE
-      },
-      dependencies: ['setup'],
-    },
-
-    {
-      name: 'firefox',
-      use: {
-        ...devices['Desktop Firefox'],
-        storageState: STORAGE_STATE
-      },
-      dependencies: ['setup'],
-    },
-
-    {
-      name: 'safari',
-      use: {
-        ...devices['Desktop Safari'],
-        storageState: STORAGE_STATE
-      },
-      dependencies: ['setup'],
-    },
-
-    {
-      name: 'ipad',
-      use: {
-        ...devices['iPad Pro 11'],
-        storageState: STORAGE_STATE
-      },
-      dependencies: ['setup'],
-    },
-
-    {
-      name: 'android',
-      use: {
-        ...devices['Galaxy S8'],
-        storageState: STORAGE_STATE
-      },
-      dependencies: ['setup'],
-    },
-
-    {
-      name: 'iphone',
-      use: {
-        ...devices['iPhone XR'],
-        storageState: STORAGE_STATE
-      },
-      dependencies: ['setup'],
-    }
   ]
-}
+});
+// import type { PlaywrightTestConfig } from '@playwright/test'
+// import { devices } from '@playwright/test'
+// import path from 'path'
+// import credsFile from '@test-data/creds.json'
 
-export default config
+// export const STORAGE_STATE = path.join(__dirname, 'test-data/storage-state.json');
+// const creds = process.env.ENV === 'staging' ? credsFile.staging : credsFile.prod
+
+// const config: PlaywrightTestConfig = {
+//   workers: 1,
+//   retries: 2,
+//   timeout: 60 * 1000,
+//   forbidOnly: true,
+//   outputDir: 'test-results/',
+//   reporter: [['html', { open: 'always', outputFolder: './test-report' }]],
+
+//   // Ignores serial notation; consider disabling this in case of flakiness
+//   fullyParallel: false,
+
+//   expect: {
+//     timeout: 15 * 1000,
+
+//     // Allowed screenshot difference; should be supervised to prevent false positives
+//     toHaveScreenshot: { maxDiffPixelRatio: 0.1 }
+//   },
+
+//   use: {
+//     actionTimeout: 15 * 1000,
+//     baseURL:
+//       process.env.ENV === 'staging'
+//         ? 'https://demo-opencart.com/'
+//         : 'https://playwright.dev/docs/intro',
+//     screenshot: 'only-on-failure',
+//     trace: 'retain-on-failure',
+//     video: 'retain-on-failure',
+
+//     viewport: { height: 1600, width: 900 },
+
+//     httpCredentials: {
+//       username: creds.http_username,
+//       password: creds.http_password
+//     },
+//   },
+
+//   projects: [
+//     {
+//       name: 'setup',
+//       testMatch: /global-setup\.ts/,
+//       teardown: 'teardown',
+//     },
+//     {
+//       name: 'teardown',
+//       testMatch: /global-teardown\.ts/,
+//       use: {
+//         storageState: STORAGE_STATE
+//       }
+//     },
+//     {
+//       name: 'chrome',
+//       use: {
+//         ...devices['Desktop Chrome'],
+//         storageState: STORAGE_STATE
+//       },
+//       dependencies: ['setup'],
+//     }
+//   ]
+// }
+
+// export default config
